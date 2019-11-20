ed908960ffc1419a6aefd2a2e1ed7ee0697f9fb9
Nishant Bharat <nishant.bharatindia@yahoo.in>
2019-11-20 23:56:22 +0530

    formula_changed

--- my_answers.py
+++ my_answers.py
@@ -90,18 +90,20 @@ class NeuralNetwork(object):
         # TODO: Output error - Replace this value with your calculations.
         error = y - final_outputs # Output layer error is the difference between desired target and actual output.
         
-        # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = self.weights_hidden_to_output*error[:,None]
-        
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = error*final_outputs*(1-final_outputs)
+        output_error_term = error 
+        
+        
+        # TODO: Calculate the hidden layer's contribution to the error
+        hidden_error = np.dot(self.weights_hidden_to_output,output_error_term)
+        
         
         hidden_error_term = hidden_error*hidden_outputs*(1-hidden_outputs)
         
         # Weight step (input to hidden)
-        delta_weights_i_h += self.lr*hidden_error_term*X[:,None]
+        delta_weights_i_h += hidden_error_term*X[:,None]
         # Weight step (hidden to output)
-        delta_weights_h_o += self.lr*output_rror_term
+        delta_weights_h_o += output_error_term*hidden_outputs[:,None]
         
         return delta_weights_i_h, delta_weights_h_o
 
@@ -115,8 +117,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += delta_weights_h_o/n_records # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += delta_weights_i_h/n_records # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += (self.lr*delta_weights_h_o)/n_records # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += (self.lr*delta_weights_i_h)/n_records # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
@@ -141,7 +143,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 2000
+learning_rate = 0.05
+hidden_nodes = 32
 output_nodes = 1
